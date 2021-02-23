f325d5634b29e8a6c37b172b6babd83ac750bb43
ssengmrng <salseng.tech@gmail.com>
2021-02-24 01:19:33 +0200

    ANN intro added

--- ANN_Intro.py
+++ ANN_Intro.py
@@ -1,13 +1,34 @@
+# Importing libraries
 from sklearn import datasets
 import matplotlib
 from matplotlib import pyplot as plt
 import numpy as np
 
+# Creating dummy dataset
 np.random.seed(0)
-feature_set, labels = datasets.make_moons(100, noise=.1)
-plt.figure(figsize=(10,7))
-plt.scatter(feature_set[:,0], feature_set[:,1], c=labels, cmap=plt.cm.winter)
-labels=labels.reshape(100,1)
+features, labels = datasets.make_moons(100, noise=.1)
+plt.figure(figsize=(10, 7))
+plt.scatter(features[:, 0], features[:, 1], c=labels, cmap=plt.cm.winter)
+labels = labels.reshape(100, 1)
 
-#plt.show()
 
+# plt.show()
+
+# Sigmoid activation function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+# Weights
+wh = np.random.rand(len(features[0]), 4)
+wo = np.random.rand(4, 1)
+lr = 0.5  # learning rate
+
+for epoch in range(20000):
+    zh = np.dot(features, wh)  # dot product of hidden layer
+    ah = sigmoid(zh)
+
+    zo = np.dot(ah, wo)
+    ao = sigmoid(zo)
+
+# Gradient descent
